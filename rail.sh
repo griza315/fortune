8332ecf47a3554df24dd08aab18d440a472e6578
griza315 <315grey@gmail.com>
2025-01-02 23:54:24 +0300

    Update rail.sh

--- rail.sh
+++ rail.sh
@@ -2,5 +2,5 @@
 
 apt update
 apt install -y sl cowsay fortune fortunes fortune-anarchism fortunes-bofh-excuses fortunes-debian-hints fortunes-spam fortunes-ubuntu-server
-echo "sl fortune | cowsay" >> .bashrc
+echo "sl \n fortune -a | cowsay" >> .bashrc
 rm -r fortune
