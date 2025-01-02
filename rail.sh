f36ec94b8d1054588afa3db5920f8371b0b02c23
griza315 <315grey@gmail.com>
2025-01-02 23:47:07 +0300

    Create rail.sh

---
+++ rail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt update
+apt install -y sl cowsay fortune fortunes fortune-anarchism fortunes-bofh-excuses fortunes-debian-hints fortunes-spam fortunes-ubuntu-server
+echo "sl fortune | cowsay" >> .bashrc
