e3e0e51c95aec5672c1c565042a722d8317c44d2
griza315 <315grey@gmail.com>
2025-01-02 23:30:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt update
-apt install fortune fortunes fortune-anarchism fortunes-bofh-excuses fortunes-debian-hints fortunes-spam fortunes-ubuntu-server
+apt install -y fortune fortunes fortune-anarchism fortunes-bofh-excuses fortunes-debian-hints fortunes-spam fortunes-ubuntu-server
 echo "fortune | saycow" >> .bashrc
